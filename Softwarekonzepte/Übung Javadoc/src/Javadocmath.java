c0af8d5d447c6784b468c1d26ae345bbd79174b6
Michael Strässle <michael.straessle@ntb.ch>
2019-09-24 13:45:09 +0200

    test

---
+++ Softwarekonzepte/Übung Javadoc/src/Javadocmath.java
@@ -0,0 +1,24 @@
+public class Javadocmath {
+    public static void main(String[] args) {
+        test();
+    }
+    /**
+     * Returns the correctly rounded....
+     * <ul>
+     *     <li>If the argument...</li>
+     *     <li>If the argument...</li>
+     *     <li>If the argument...</li>
+     *
+     * </ul>
+     * otherwise, the result....
+     *
+     * <h1>Parameters:</h1>
+     * a - a value
+     * <h1>Returns:</h1>
+     * the positive root....
+     */
+    private static void test(){
+
+        System.out.println("Test");
+    }
+}
